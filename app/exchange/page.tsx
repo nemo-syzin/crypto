9c70e4e4ec47c48c0772111db8aac54c00cfbfe4
nemo-syzin <153398240+nemo-syzin@users.noreply.github.com>
2025-06-26 14:24:56 +0200

    Updated page.tsx

--- app/exchange/page.tsx
+++ app/exchange/page.tsx
@@ -181,7 +181,7 @@ export default function ExchangePage() {
               <div className="order-2 lg:order-1">
                 <div className="relative mx-auto" style={{ 
                   width: '560px', 
-                  height: '960px',
+                  height: '560px',
                   maxWidth: '100%'
                 }}>
                   {/* Компактное видео в фиксированном контейнере */}
