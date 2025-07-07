6f9bb3d752a5087e412613b140dc368a75269b3c
nemo-syzin <153398240+nemo-syzin@users.noreply.github.com>
2025-07-07 16:32:22 +0200

    Fix Next.js image domain configuration

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['assets.revolut.com'],
+  },
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
