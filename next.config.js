b62424fb454c2d03c499463e37d6f6708003e822
nemo-syzin <153398240+nemo-syzin@users.noreply.github.com>
2025-07-07 16:44:06 +0200

    Update Next.js Config for Image Domains

--- next.config.js
+++ next.config.js
@@ -8,29 +8,51 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "cdn.brandfetch.io",
+        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "images.pexels.com",
+        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "coex.global",
+        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "assets.revolut.com",
+        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "www.revolut.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "assets.coingecko.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "res.coinpaper.com",
+        pathname: "/**",
       }
     ],
   },
   // Optimize bundle size
   experimental: {
     optimizePackageImports: ['lucide-react', 'framer-motion']
-  }
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname, '.'),
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
