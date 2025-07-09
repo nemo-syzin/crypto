cec6af5b7df31c71a462367847fd51d880d84bf4
nemo-syzin <153398240+nemo-syzin@users.noreply.github.com>
2025-07-09 14:23:58 +0200

    Add missing RUB/USDT exchange rate

---
+++ supabase/migrations/20250709122352_smooth_cave.sql
@@ -0,0 +1,6 @@
+@@ .. @@
+   -- USDC pairs
+   ('kenig', 'USDC', 'USDT', 1.002, 0.998, now()),
+   ('kenig', 'USDC', 'RUB', 95.20, 94.60, now()),
++  ('kenig', 'RUB', 'USDT', 0.0106, 0.0105, now()),
+   ('kenig', 'USDC', 'BTC', 0.000034, 0.000033, now()),
