673cd4b2142197f404ae75385157b6a898321411
nemo-syzin <153398240+nemo-syzin@users.noreply.github.com>
2025-07-10 16:23:04 +0200

    Updated footer.tsx

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -73,15 +73,7 @@ const Footer = () => {
                       Связаться с менеджером
                     </a>
                   </li>
-                © {new Date().getFullYear()} KenigSwap. All rights reserved.
-              </div>
-              <div className="flex items-center gap-4 mt-4 md:mt-0">
-                <Link href="/login" className="text-sm text-gray-500 hover:text-[#001D8D]">
-                  Sign In
-                </Link>
-                <Link href="/get-started" className="text-sm text-gray-500 hover:text-[#001D8D]">
-                  Get Started
-                </Link>
+                </ul>
               </div>
             </div>
           </div>
@@ -141,6 +133,14 @@ const Footer = () => {
             <p className="text-sm text-gray-500">
               © {new Date().getFullYear()} Kenigswap. All rights reserved.
             </p>
+            <div className="flex items-center gap-4 mt-4 md:mt-0">
+              <Link href="/login" className="text-sm text-gray-500 hover:text-[#001D8D]">
+                Sign In
+              </Link>
+              <Link href="/get-started" className="text-sm text-gray-500 hover:text-[#001D8D]">
+                Get Started
+              </Link>
+            </div>
           </div>
         </div>
       </div>
