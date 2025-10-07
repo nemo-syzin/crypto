9c78f7e17eb968abd3cce6609ba60ddd60137599
nemo-syzin <153398240+nemo-syzin@users.noreply.github.com>
2025-10-07 15:59:48 +0200

    Updated layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,6 +59,38 @@ export default function RootLayout({
         <link rel="dns-prefetch" href="//assets.coingecko.com" />
         <link rel="dns-prefetch" href="//res.coinpaper.com" />
         <link rel="dns-prefetch" href="//api.alternative.me" />
+        <link rel="dns-prefetch" href="//mc.yandex.ru" />
+
+        {/* Yandex.Metrika counter */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js', 'ym');
+
+              ym(104446016, 'init', {
+                ssr:true,
+                webvisor:true,
+                clickmap:true,
+                ecommerce:"dataLayer",
+                accurateTrackBounce:true,
+                trackLinks:true
+              });
+            `,
+          }}
+        />
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/104446016" style={{position:'absolute', left:'-9999px'}} alt="" />
+          </div>
+        </noscript>
+        {/* /Yandex.Metrika counter */}
+
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
