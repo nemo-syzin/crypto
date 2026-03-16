6bd8774bfd95a933cffab5e9f04bf8dfa0b3ed08
nemo-syzin <153398240+nemo-syzin@users.noreply.github.com>
2026-03-16 17:32:55 +0200

    Update hero-section.tsx

--- components/home/hero-section.tsx
+++ components/home/hero-section.tsx
@@ -26,7 +26,7 @@ const HeroSection = () => {
             </div>
 
             <div>
-              <Link href="/exchange">
+              <Link href="https://kenigswap.ru/exchange/">
                 <Button
                   size="lg"
                   className="bg-[#001D8D] hover:bg-[#001D8D]/90 text-white px-8 py-4 text-lg font-semibold shadow-lg hover:shadow-xl hover:scale-105 active:scale-95 transition-all duration-300"
